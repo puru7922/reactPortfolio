7f9a85e3e7fe0a2832f8214f21f8206c9688e9cf
Purushottam Kumar <purushottamkumar@Purushottams-MacBook.local>
2024-01-07 00:02:20 +0530

    services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,14 @@ import './services.css'
 const Services = () => {
     return (
         <section id='services'>
-            Services
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div>
+                <div className='container service'>
+
+                </div>
+            </div>
         </section>
     )
 }
